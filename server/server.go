f89c3c3723c2204ec07c9dae740b62bd210f67da
Jamie Hall <the.sly.marbo@googlemail.com>
2013-04-20 13:47:47 +0100

    Minor fix

--- server/server.go
+++ server/server.go
@@ -2,12 +2,13 @@ package server
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-type HTTPRedirector struct {}
+type HTTPRedirector struct{}
 
 func (_ HTTPRedirector) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	url := r.URL
@@ -24,6 +25,25 @@ func serveHTTP(domain string) {
 }
 
 
+type Nexus struct{}
+
+func (_ Nexus) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/", "/index.html":
+		return ServeMain(w, r)
+		
+	case "/login":
+		return ServeLogin(w, r)
+		
+	default:
+		return NotFound(w, r)
+	}
+}
+
+
 func serveHTTPS(domain, cert, key string) {
-	
+	err := http.ListenAndServeTLS(domain, cert, key, Nexus{})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
