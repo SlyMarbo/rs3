aaac358ba5b06d6db77312a53b2857ce412345f8
Jamie Hall <the.sly.marbo@googlemail.com>
2013-04-20 11:56:49 +0100

    Added HTTP redirection

--- server/server.go
+++ server/server.go
@@ -1,3 +1,29 @@
 package server
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
+type HTTPRedirector struct {}
+
+func (_ HTTPRedirector) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	url := r.URL
+	url.Scheme = “https”
+	http.Redirect(w, r, url.String(), 301)
+}
+
+func serveHTTP(domain string) {
+	for {
+		err := http.ListenAndServe(domain, HTTPRedirector{})
+		fmt.Fprintf(os.Stderr, err)
+		time.Sleep(10 * time.Second)
+	}
+}
+
+
+func serveHTTPS(domain, cert, key string) {
+	
+}
